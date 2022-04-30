1c9775ee202f72eee653d154246e261a668d2b27
Anuj Maurya <assccii@outlook.com>
2022-04-30 13:09:21 +0530

    new

--- src/components/follow_suggestion.js
+++ src/components/follow_suggestion.js
@@ -3,7 +3,30 @@ import React from "react"
 export default function FollowSuggestion() {
     return (
         <div className="follow-suggestion">
-            <h1>this is follow-suggestion</h1>
+           <div className="recent-searches">
+                <h2 className="recent-searches-text">Recent Searches</h2>
+                <ul>
+                    <li>
+                        <img src = "#" alt = " "/>
+                        UX Design 
+                    </li>
+
+                    <li>
+                        <img src = "#" alt = " "/>
+                        Marketing
+                    </li>
+
+                    <li>
+                        <img src = "#" alt = " "/>
+                        Expansion
+                    </li>
+
+                    <li>
+                        <img src = "#" alt = " "/>
+                        Technology
+                    </li>
+                </ul>
+           </div>
         </div>
     )
 
