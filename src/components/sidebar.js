72f4fe7c12034962b07f87ac12532fa46da6524d
Anuj Maurya <assccii@outlook.com>
2022-04-30 13:09:36 +0530

    new

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -3,7 +3,66 @@ import React from "react"
 export default function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>this is sidebar</h1>
+            <div className="list1">
+                <ul>
+                    <li className="first"> 
+                        <img src="#" alt = ""/>
+                          Hot Scopes
+                    </li>
+                    <li><img src="#" alt = ""/>
+                        Earnings
+                    </li>
+                    <li>
+                        <img src="#" alt = ""/>
+                        Awards
+                    </li>
+                    <li>
+                         <img src="#" alt = ""/> 
+                         Expansions
+                    </li>
+                    <li>
+                        <img src="#" alt = ""/>
+                        Open Positions
+                    </li>
+                    <li>
+                        <img src="#" alt = ""/>
+                        New Hire
+                    </li>
+                </ul>
+            </div>
+            <div className="list2">
+                <ul>
+                    <li>
+                        <img url="#" alt = ""/>
+                        Analytics & Reporting
+                    </li>
+                    <li>
+                        <img url="#" alt = ""/>
+                        Budgeting & Planning
+                    </li>
+                    <li>
+                    <img url="#" alt = ""/>
+                        Communications
+                    </li>
+                    <li>
+                    <img url="#" alt = ""/>
+                        Technology
+                    </li>
+                    <li>
+                    <img url="#" alt = ""/>
+                        Legal
+                    </li>
+                    <li>
+                        <img url="#" alt = ""/>
+                        Infrastructure
+                    </li>
+                    <li>
+                        <img url="#" alt = ""/>
+                        Marketing
+                    </li>
+                    
+                </ul>
+            </div>
         </div>
     )
 }
